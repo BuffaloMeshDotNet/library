aafc7601ead2f3abebbf40c5de305ce44607afb4
Corey Reichle <corey.reichle@gmail.com>
2015-02-04 10:29:41 -0500

    Not doing the fancy pants Vanilla JS thing for now

--- index.php
+++ index.php
@@ -3,13 +3,34 @@
 		<link rel="stylesheet" href="styles.css">
 	</head>
 	<body>
-		<div class="booklist">
-			<a href="#hide1" class="hide" id="hide1">+</a>
-			<a href="#show1" class="show" id="show1">-</a>
-			<div class="booktype"> ePub Format </div>
-			<div class="list">
-				<p>Put PHP code here </p>
-			</div>
-		</div>
+	<details>
+	<summary>ePub</summary>
+		<?php
+		$myDirectory = opendir("./epub");
+
+		while($entryName = readdir($myDirectory)) {
+			$dirArray[] = $entryName;
+		}
+
+// close directory
+		closedir($myDirectory);
+
+//	count elements in array
+		$indexCount	= count($dirArray);
+		Print ("$indexCount files<br>\n");
+
+// sort 'em
+		sort($dirArray);
+
+// print 'em
+// loop through the array of files and print them all
+		for($index=0; $index < $indexCount; $index++) {
+			if (substr("$dirArray[$index]", 0, 1) != "."){ // don't list hidden files
+				print("<p><a href=\"$dirArray[$index]\">$dirArray[$index]</a></p>");
+			}
+		}
+
+		?>
+		</details>
 	</body>
 </html>
