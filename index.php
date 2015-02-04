0f4f05773b8c9a785511ff18fc7fb017d013555c
Corey Reichle <corey.reichle@gmail.com>
2015-02-04 11:26:34 -0500

    All formats included

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 		<link rel="stylesheet" href="styles.css">
 	</head>
 	<body>
+
 	<details>
 		<summary>ePub</summary>
 		<?php
@@ -19,5 +20,58 @@
 		?>
 		<p><?=$thelist?></p>	
 	</details>
+
+	<details>
+		<summary>mobi</summary>
+		<?php
+			if ($handle = opendir('mobi/')) {
+				while (false !== ($file = readdir($handle)))
+				{
+					if ($file != "." && $file != "..")
+					{
+						$thelist .= '<a href="mobi/'.$file.'">'.$file.'</a></br>';
+          		}
+       		}
+			closedir($handle);
+			}       
+		?>
+		<p><?=$thelist?></p>	
+	</details>
+
+	<details>
+		<summary>PDF</summary>
+		<?php
+			if ($handle = opendir('pdf/')) {
+				while (false !== ($file = readdir($handle)))
+				{
+					if ($file != "." && $file != "..")
+					{
+						$thelist .= '<a href="pdf/'.$file.'">'.$file.'</a></br>';
+          		}
+       		}
+			closedir($handle);
+			}       
+		?>
+		<p><?=$thelist?></p>	
+	</details>
+
+	<details>
+		<summary>Misc Formats</summary>
+		<?php
+			if ($handle = opendir('misc/')) {
+				while (false !== ($file = readdir($handle)))
+				{
+					if ($file != "." && $file != "..")
+					{
+						$thelist .= '<a href="misc/'.$file.'">'.$file.'</a></br>';
+          		}
+       		}
+			closedir($handle);
+			}       
+		?>
+		<p><?=$thelist?></p>	
+	</details>
+
 	</body>
 </html>
+
