5a5928dc07c7be6d7a37c9a46643db60b70dfb77
Corey Reichle <corey.reichle@gmail.com>
2015-02-04 11:00:02 -0500

    Getting there, just working on some weirdness with the URI being generated...

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
 // loop through the array of files and print them all
 		for($index=0; $index < $indexCount; $index++) {
 			if (substr("$dirArray[$index]", 0, 1) != "."){ // don't list hidden files
-				print("<p><a href=\"$dirArray[$index]\">$dirArray[$index]</a></p>");
+				print("<p><a href=/epub/".$dirArray[$index].">".$dirArray[$index]."</a></p>");
 			}
 		}
 
