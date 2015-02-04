11a20bedf8ac1acea401ef57b954cdc7a8273404
Corey Reichle <corey.reichle@gmail.com>
2015-02-04 09:38:38 -0500

    Making POC framework

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
-<? php
-print("Hello world");
-
-?>
+<html>
+	<head>
+	</head>
+	<body>
+		<div class="booklist">
+			<a href="#hide1" class="hide" id="hide1">+</a>
+			<a href="#show1" class="show" id="show1">-</a>
+			<div class="booktype"> ePub Format </div>
+			<div class="list">
+				<p>Put PHP code here </p>
+			</div>
+		</div>
+	</body>
+</html>
