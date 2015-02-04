76e5b749570cdefe016583d996251e2b21f2f683
Corey Reichle <corey.reichle@gmail.com>
2015-02-04 12:47:08 -0500

    Some formatting

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 	If there are any titles you'd like, please let the node administrator know, and we'll try our best to locate it, and get it up here for you.</p>
 	<div id="ePub" class="col">
 		<h2>ePub</h2><br/>
-		Most devices can read this.<br/>
+		Most devices can read this.<p/>
 			<?php
 				if ($handle = opendir('epub/')) {
 					while (false !== ($file = readdir($handle)))
@@ -27,7 +27,7 @@
 
 	<div id="mobi" class="col">
 		<h2>mobi Format</h2><br/>
-		Amazon devices (aka Kindles)<br/>
+		Amazon devices (aka Kindles)</p>
 			<?php
 				if ($handle = opendir('mobi/')) {
 					while (false !== ($file = readdir($handle)))
@@ -45,7 +45,7 @@
 
 	<div id="pdf" class="col">
 			<h2>PDF Format</h2><br/>
-			Laptops and Desktops<br/>
+			Laptops and Desktops<p/>
 			<?php
 				if ($handle = opendir('pdf/')) {
 					while (false !== ($file = readdir($handle)))
@@ -63,7 +63,7 @@
 
 	<div id="misc" class="col">
 			<h2>Misc Formats</h2><br/>
-			Various formats (Text files, etc)
+			Various formats (Text files, etc)<p/>
 			<?php
 				if ($handle = opendir('misc/')) {
 					while (false !== ($file = readdir($handle)))
